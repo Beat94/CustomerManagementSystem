216b2f814b913a2b00e9f7122e36995f1bf17098
Beat94 <36741096+Beat94@users.noreply.github.com>
2022-06-29 22:17:28 +0200

    Adding class

--- Log.java
+++ Log.java
@@ -1,8 +1,19 @@
+import java.time.LocalDateTime;
+
 public class Log{
     private String text;
     private LocalDateTime datumZeit;
 
     public Log(String text){
         this.text = text;
+        datumZeit = LocalDateTime.now();
+    }
+
+    public String getText(){
+        return this.text;
+    }
+
+    public LocalDateTime getDatumZeit(){
+        return this.datumZeit;
     }
 }
