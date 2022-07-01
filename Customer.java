5447999447265eef1a4a3962d145b46d93f4def6
Beat94 <36741096+Beat94@users.noreply.github.com>
2022-07-01 19:18:10 +0200

    Anpassung Customer

--- Customer.java
+++ Customer.java
@@ -2,13 +2,19 @@ import java.util.ArrayList;
 
 public class Customer{
     private String name;
-    private ArrayList<Log> cLog = new ArrayList<Log>;
+    private ArrayList<Log> cLog = new ArrayList<Log>();
     private Adresse address;
 
     public Customer(String name, String strasseNr, String ort, String plz, String land){
         this.name = name;
-        this.address = new Adresse(strasseNr, ort, plz, land)
+        this.address = new Adresse(strasseNr, ort, plz, land);
+        addLog("Die Firma " + name + " ist angelegt.");
     }
+
+    public void addLog(String note){
+        this.cLog.add(new Log(note));
+    }
+
 
     
 }
