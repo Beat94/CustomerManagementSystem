ef89b2b2c512eaffcae268867bda774766300464
Beat94 <36741096+Beat94@users.noreply.github.com>
2022-07-03 12:30:10 +0200

    create CustomerManager - adding functions on Customer

--- Customer.java
+++ Customer.java
@@ -4,17 +4,33 @@ public class Customer{
     private String name;
     private ArrayList<Log> cLog = new ArrayList<Log>();
     private Adresse address;
+    private boolean active;
 
     public Customer(String name, String strasseNr, String ort, String plz, String land){
         this.name = name;
         this.address = new Adresse(strasseNr, ort, plz, land);
         addLog("Die Firma " + name + " ist angelegt.");
+        this.active = true;
     }
 
     public void addLog(String note){
         this.cLog.add(new Log(note));
     }
 
+    public String getName(){
+        return this.name;
+    }
 
-    
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public boolean getActive(){
+        return this.active;
+    }
+
+    public void setActive(boolean active){
+        this.active = active;
+    }
+
 }
---
+++ CustomerManager.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class CustomerManager{
+    private ArrayList<Customer> customerArr = new ArrayList<Customer>();
+    
+
+
+}
