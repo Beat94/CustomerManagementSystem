9b649034aa20902e023b162f8c0b8ce55283838c
Beat94 <36741096+Beat94@users.noreply.github.com>
2022-07-15 09:41:51 +0200

    added function

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 class Main {
-    public static void main(String[]arcs){
+
+    private static void startUi(String UI){
+        if(UI.toLowerCase().equals("cli")){
+            System.out.println("CLI");
+        }else if(UI.toLowerCase().equals("gui")){
+            System.out.println("GUI")
+        }
+    }
+
+    public static void main(String[] arcs) {
         CustomerManager cm = new CustomerManager();
 
     }
