5c67c307e116b21098ceae31934a759a1d2dfc74
Beat94 <36741096+Beat94@users.noreply.github.com>
2022-07-20 00:33:58 +0200

    Added Startup Dialogue

--- Main.java
+++ Main.java
@@ -1,15 +1,37 @@
+import javax.swing.*;
+
 class Main {
 
     private static void startUi(String UI){
         if(UI.toLowerCase().equals("cli")){
             System.out.println("CLI");
         }else if(UI.toLowerCase().equals("gui")){
-            System.out.println("GUI")
+            System.out.println("GUI");
         }
     }
 
     public static void main(String[] arcs) {
         CustomerManager cm = new CustomerManager();
 
+        //Start-GUI
+        JFrame frame = new JFrame("Start-");
+
+        JButton gui = new JButton("GUI");
+        JButton cli = new JButton("CLI");
+
+        JLabel quest = new JLabel("Wie soll die App geöffnet werden?");
+
+        gui.setBounds(70, 90, 100, 40); //x axis, y axis, width, height
+        cli.setBounds(180, 90, 100, 40); //x axis, y axis, width, height
+        quest.setBounds(70, 20,500,40);
+
+        frame.add(gui);
+        frame.add(cli);
+        frame.add(quest);
+
+        frame.setSize(400,200);
+
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
 }
