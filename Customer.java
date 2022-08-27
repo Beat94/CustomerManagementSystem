c650ede1d78cb3c1551252d62f2907d1b749a27f
Beat Giuliano Rey <beat_g_94@hotmail.com>
2022-08-27 15:12:49 +0200

    correction - Customer.java

--- Customer.java
+++ Customer.java
@@ -1,20 +1,15 @@
 import java.util.ArrayList;
 
-public class Customer {
+public class Customer{
     private String name;
-    private ArrayList<Log> cLog = new ArrayList<Log>();
+    private ArrayList<Log> cLog = new ArrayList<Log>;
     private Adresse address;
     private boolean active;
 
-    public Customer(String name, Adresse address) {
+    public Customer(String name, String strasseNr, String ort, String plz, String land){
         this.name = name;
-        this.address = address;
-        addLog("Die Firma " + name + " ist angelegt.");
+        this.address = new Adresse(strasseNr, ort, plz, land);
         this.active = true;
-    }
-
-    public void addLog(String note) {
-        this.cLog.add(new Log(note));
     }
 
     public String getName() {
