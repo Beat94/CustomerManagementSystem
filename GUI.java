f17e31c31d55a1ba89b08216609e39f91df5ee55
Beat94 <36741096+Beat94@users.noreply.github.com>
2022-08-27 20:54:01 +0200

    Fix Startup and Add another gui

--- GUI.java
+++ GUI.java
@@ -1,3 +1,31 @@
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JViewport;
+
 public class GUI {
-    
+    public void start(){
+        JFrame frame = new JFrame("Customer Management System");
+
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu options = new JMenu("Options");
+
+        JMenuItem save = new JMenuItem("save");
+        JMenuItem load = new JMenuItem("load");
+
+        options.add(save);
+        options.add(load);
+
+        menuBar.add(options);
+
+        frame.setJMenuBar(menuBar);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,200);
+
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
 }
--- Startup.java
+++ Startup.java
@@ -1,8 +1,10 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class Startup {
     public void start(){
-        JFrame frame = new JFrame("Start-");
+        JFrame frame = new JFrame("Startup-Method");
 
         JButton gui = new JButton("GUI");
         JButton cli = new JButton("CLI");
@@ -13,21 +15,23 @@ public class Startup {
         cli.setBounds(180, 90, 100, 40); //x axis, y axis, width, height
         quest.setBounds(70, 20,500,40);
 
+        gui.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                GUI gui = new GUI();
+                gui.start();
+                frame.dispose();
+            }
+        });
+
         frame.add(gui);
         frame.add(cli);
         frame.add(quest);
 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400,200);
 
         frame.setLayout(null);
         frame.setVisible(true);
-    }
-
-    private static void startUi(String UI){
-        if(UI.toLowerCase().equals("cli")){
-            System.out.println("CLI");
-        }else if(UI.toLowerCase().equals("gui")){
-            System.out.println("GUI");
-        }
     }
 }
