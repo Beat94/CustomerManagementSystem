ecd815d2e35f97f8d8dd7ec4bd3bf0f540ed8b90
Beat94 <36741096+Beat94@users.noreply.github.com>
2022-08-28 02:25:04 +0200

    gui configs

--- GUI.java
+++ GUI.java
@@ -1,10 +1,12 @@
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JViewport;
+import javax.swing.text.JTextComponent;
 
 public class GUI {
     public void start(){
@@ -16,7 +18,7 @@ public class GUI {
         // JComboBox: https://docs.oracle.com/javase/tutorial/uiswing/components/combobox.html
         JComboBox customerChooser = new JComboBox();
         JButton changeCustomerNameButton = new JButton("Name ändern");
-        
+        JLabel activeText = new JLabel("Aktiv:");
 
         options.add(save);
         options.add(load);
@@ -25,10 +27,12 @@ public class GUI {
 
         frame.add(customerChooser);
         frame.add(changeCustomerNameButton);
+        frame.add(activeText);
         frame.setJMenuBar(menuBar);
         
         customerChooser.setBounds(10, 20, 150, 25);
         changeCustomerNameButton.setBounds(170, 20, 150, 25);
+        activeText.setBounds(10, 55, 70, 25);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400,200);
