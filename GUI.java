1174aca025dcf181e54895a17b4a78d7384afb47
Beat94 <36741096+Beat94@users.noreply.github.com>
2022-08-28 02:09:32 +0200

    gui-changes

--- GUI.java
+++ GUI.java
@@ -1,3 +1,5 @@
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -7,24 +9,30 @@ import javax.swing.JViewport;
 public class GUI {
     public void start(){
         JFrame frame = new JFrame("Customer Management System");
-
         JMenuBar menuBar = new JMenuBar();
-
         JMenu options = new JMenu("Options");
-
         JMenuItem save = new JMenuItem("save");
         JMenuItem load = new JMenuItem("load");
+        // JComboBox: https://docs.oracle.com/javase/tutorial/uiswing/components/combobox.html
+        JComboBox customerChooser = new JComboBox();
+        JButton changeCustomerNameButton = new JButton("Name ändern");
+        
 
         options.add(save);
         options.add(load);
 
         menuBar.add(options);
 
+        frame.add(customerChooser);
+        frame.add(changeCustomerNameButton);
         frame.setJMenuBar(menuBar);
+        
+        customerChooser.setBounds(10, 20, 150, 25);
+        changeCustomerNameButton.setBounds(170, 20, 150, 25);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400,200);
-
+        
         frame.setLayout(null);
         frame.setVisible(true);
     }
